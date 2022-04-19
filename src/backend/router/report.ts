5248288ab2567eee65916914630fc6f6ae1be14e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-19 20:37:11 +0200

    playing around with react query

--- src/backend/router/report.ts
+++ src/backend/router/report.ts
@@ -1,35 +1,51 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
 import { prisma } from "~/backend/db/client";
 
 export const reportRouter = trpc
   .router()
-  .query('get-all', {
+  .query("get-all", {
     async resolve() {
-      return await prisma.report.findMany();
+      return await prisma.report.findMany({
+        select: {
+          id: true,
+          description: true,
+          measurement: true,
+        },
+      });
     },
   })
-  .query("get-by-id", { 
-    input: z.object({ 
-      id: z.string() 
+  .query("get-by-id", {
+    input: z.object({
+      id: z.string(),
     }),
     async resolve({ input }) {
       return await prisma.report.findFirst({
-        where: { 
-          id: input.id 
+        where: {
+          id: input.id,
         },
       });
-    }
+    },
   })
-  .mutation("create", { 
-    input: z.object({ 
-      description: z.string().min(5).max(100)
+  .mutation("create", {
+    input: z.object({
+      description: z.string().min(5).max(100),
+      measurements: z.array(
+        z.object({
+          value: z.number(),
+          type: z.string(),
+        })
+      ),
     }),
     async resolve({ input }) {
-      return await prisma.report.create({
+      const report = await prisma.report.create({
         data: {
-          description: input.description
-        }
-      })
-    }
+          description: input.description,
+        },
+      });
+      const measurements = await prisma.measurement.createMany({
+        data: input.measurements.map((m) => ({ ...m, reportId: report.id })),
+      });
+      return report;
+    },
   });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,44 @@
 import { trpc } from "~/utils/trpc";
+import React from "react";
 
-export default function Home () {
+export default function Home() {
   const { data, isLoading } = trpc.useQuery(["reports.get-all"]);
+  const client = trpc.useContext();
+  const { mutate } = trpc.useMutation("reports.create", {
+    onSuccess: (data) => {
+      console.log("created with:", data);
+      client.invalidateQueries(["reports.get-all"]);
+    },
+  });
 
   if (isLoading || !data) return <div>Loading...</div>;
 
+  console.log(data);
   return (
     <div className="flex flex-col p-8">
-      <h2 className="text-2xl">Reports:</h2>
-      {data.map((report) => {
-        return (
-          <div key={report.id}>
-            {report.description}
-          </div>
-        );
-      })}
+      <div className="">
+        <h2 className="text-2xl">Reports:</h2>
+        {data.map((report) => {
+          return <div key={report.id}>{report.description}</div>;
+        })}
+      </div>
+      <div className="">
+        <button
+          className=""
+          onClick={() => {
+            const values = ["temperature", "humidity"].map((type) => {
+              const value = Math.random() * 100;
+              return { type, value };
+            });
+            mutate({
+              measurements: values,
+              description: "Sunny Weather Today!",
+            });
+          }}
+        >
+          Create report
+        </button>
+      </div>
     </div>
-  )
+  );
 }
