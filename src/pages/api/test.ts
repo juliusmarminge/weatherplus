e0505dfbc931a141134427880584ddd0f405b8f4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-26 17:15:48 +0200

    test dummy api endpoint

---
+++ src/pages/api/test.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body, headers, query } = req;
+  console.log("method", method);
+  console.log("body", body);
+  console.log("headers", headers);
+  console.log("query", query);
+  res.status(200).json({ method, body, headers, query });
+};
